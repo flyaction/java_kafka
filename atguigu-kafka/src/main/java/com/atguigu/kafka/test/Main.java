41b3644eb886772bf87e73dc011194093e50265a
action <xingdong1117@126.com>
2025-04-08 14:29:15 +0800

    feat:project-init

---
+++ atguigu-kafka/src/main/java/com/atguigu/kafka/test/Main.java
@@ -0,0 +1,11 @@
+package com.atguigu.kafka.test;
+
+/**
+ * @author: action
+ * @create: 2025/4/8 14:24
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ atguigu-kafka/src/main/java/com/atguigu/kafka/test/producer/KafkaProducerTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.kafka.test.producer;
+
+/**
+ * @author: action
+ * @create: 2025/4/8 14:25
+ **/
+public class KafkaProducerTest {
+    public static void main(String[] args) {
+
+    }
+}
