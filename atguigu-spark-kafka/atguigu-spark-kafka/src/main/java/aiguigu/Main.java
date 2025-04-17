4e02302b51d290b5ee35b431a388eb1653068090
action <xingdong1117@126.com>
2025-04-17 11:11:03 +0800

    feat:spark-kafka

---
+++ atguigu-spark-kafka/atguigu-spark-kafka/src/main/java/aiguigu/Main.java
@@ -0,0 +1,11 @@
+package aiguigu;
+
+/**
+ * @author: action
+ * @create: 2025/4/17 10:32
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
