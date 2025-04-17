1a25ac0b3cdf29c26ae938caf0891031950d1764
action <xingdong1117@126.com>
2025-04-17 12:18:56 +0800

    feat:kafka-flink

---
+++ atguigu-flink-kafka/atguigu-flink-kafka/src/main/java/org/atguigu/Main.java
@@ -0,0 +1,11 @@
+package org.atguigu;
+
+/**
+ * @author: action
+ * @create: 2025/4/17 11:22
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
